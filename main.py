7f70e9837000156075bdad93b4a31235bf99a4ae
Derek Lance <derekwarnerlance@yahoo.com>
2018-09-24 17:57:05 -0700

    Implement candidateGen() function (not fully tested)

---
+++ main.py
@@ -0,0 +1,34 @@
+from itertools import combinations
+
+# Set F of frequent itemsets of size k
+# Returns an array of candidate itemsets of size k + 1
+def candidateGen(F, k):
+	C = set()
+
+	for f1, f2 in combinations(F, 2):
+		c = f1.union(f2)
+		flag = False
+		
+		if len(c) == k + 1:
+			flag = True
+		for s in combinations(c, len(c) - 1):
+			if not set(s) in F:
+				flag = False
+		if flag == True:
+			C.add(c)
+
+	return C
+
+def main():
+	F = {
+		frozenset(['a', 'b', 'c']),
+		frozenset(['a', 'b', 'd']),
+		frozenset(['a', 'c', 'd']),
+		frozenset(['b', 'c', 'd'])
+	}
+	print(candidateGen(F, 3))
+
+if __name__ == "__main__":
+	main()
+	F = {'1', '2'}
+	f = {'1'}
