963887477b694eb23b01d8944c8eeee8f8fbf574
Pat-Farrell-17 <38262281+Pat-Farrell-17@users.noreply.github.com>
2018-10-01 19:32:40 -0700

    Skyline itemsets removes singletons now

--- assocRules.py
+++ assocRules.py
@@ -39,7 +39,7 @@ def candidateGen(F, k):
 #           frequent itemset and the value is the support of that itemset
 def apriori(T, I, minSup):
 
-    # Generate frequent itemsets of size 1
+	# Generate frequent itemsets of size 1
 	frequentItemsets = {}
 	F = [frozenset({i}) for i in I if support(T, frozenset({i})) >= minSup]
 	for fq in F:
@@ -47,11 +47,11 @@ def apriori(T, I, minSup):
 	k = 2
 	n = len(T)
 
-    # While there are still possible candidates
+	# While there are still possible candidates
 	while F:
 
-    	# Set up working copy of gen k frequent itemsets and get the candidates
-        # Initialize the counts for each generated candidate
+		# Set up working copy of gen k frequent itemsets and get the candidates
+		# Initialize the counts for each generated candidate
 		f = set(F)
 		candidates = candidateGen(f, k-1)
 		count = [0 for i in range(len(candidates))]
@@ -63,14 +63,14 @@ def apriori(T, I, minSup):
 				if candidate.issubset(marketbasket):
 					count[i] += 1
 
-        # Set F to be all candidates in the current gen s.t. the support of the
-        # candidate is greater than or equal to the specified minimum support
-        # value
+		# Set F to be all candidates in the current gen s.t. the support of the
+		# candidate is greater than or equal to the specified minimum support
+		# value
 		F = [candidate for i, candidate in enumerate(candidates) if (count[i] / n) >= minSup ]
 
 
-        # Add gen k to the master frequentItemsets dict with its support value
-        # and increment the gen counter, k
+		# Add gen k to the master frequentItemsets dict with its support value
+		# and increment the gen counter, k
 		for fq in F:
 			frequentItemsets[fq] = (support(T, fq))
 		k += 1
@@ -82,67 +82,69 @@ def apriori(T, I, minSup):
 # Ouput:    A skyline collection of frequent itemsets of F          (dict)
 #           i.e. removes all sets in F that are a subset of another set in F
 def skylineFrequentItemsets(F):
-    keysT = list(F.keys()).copy()
-    keys = keysT.copy()
+	keysT = list(F.keys()).copy()
+	keys = keysT.copy()
 
 
-    fD = {}
-    for itmsetA in keys:
-        for itmsetB in keys:
-            if not (itmsetA == itmsetB) and itmsetA.issubset(itmsetB):
-                if itmsetA in keysT:
-                    keysT.remove(itmsetA)
-    for skyline in keysT:
-        fD[skyline] = F[skyline]
+	fD = {}
+	for itmsetA in keys:
+		if (len(itmsetA) == 1):
+			keysT.remove(itmsetA)
+		for itmsetB in keys:
+			if not (itmsetA == itmsetB) and itmsetA.issubset(itmsetB):
+				if itmsetA in keysT:
+					keysT.remove(itmsetA)
+	for skyline in keysT:
+		fD[skyline] = F[skyline]
 
-    return fD
+	return fD
 
 
 # Input:    A collection of marketbaskets T and a specific itemset items
 # Ouput:    The support value of items in T (% of all marketbaskets in T
 #           containing items)
 def support(T, items):
-    numBaskets = 0
-    for marketbasket in T:
-        if items.issubset(marketbasket):
-            numBaskets += 1
-    return (numBaskets / len(T))
+	numBaskets = 0
+	for marketbasket in T:
+		if items.issubset(marketbasket):
+			numBaskets += 1
+	return (numBaskets / len(T))
 
 
 def main():
 
-    print("\n\nThis program should be run with the following arguments:\n\n\t assocRules.py [input filename] [min sup] [min conf] [output filename] [data set]")
-    file = sys.argv[1]
-    minSup = float(sys.argv[2])
-    minConf = float(sys.argv[3])
-    writeFile = sys.argv[4]
-    dataSet = sys.argv[5]
+	print("\n\nThis program should be run with the following arguments:\n\n\t assocRules.py [input filename] [min sup] [min conf] [output filename] [data set]")
+	file = sys.argv[1]
+	minSup = float(sys.argv[2])
+	minConf = float(sys.argv[3])
+	writeFile = sys.argv[4]
+	dataSet = sys.argv[5]
 
 
 
-    print("\nRunning association rules mining on\n\n\t{} with a minSup of\t{}\t and a minConf of\t{}".format(file, minSup, minConf))
+	print("\nRunning association rules mining on\n\n\t{} with a minSup of\t{}\t and a minConf of\t{}".format(file, minSup, minConf))
 
-    T = data.readDataInput(file)
+	T = data.readDataInput(file)
 
-    if dataSet.lower() == "bakery":
+	if dataSet.lower() == "bakery":
 
-        items = data.buildGoods("goods.csv")
+		items = data.buildGoods("goods.csv")
 
-        fqItmsets = apriori(T, range(len(items)), minSup)
-        skyFQ = skylineFrequentItemsets(fqItmsets)
+		fqItmsets = apriori(T, range(len(items)), minSup)
+		skyFQ = skylineFrequentItemsets(fqItmsets)
 
-        data.writeBakeryOutput(writeFile, skyFQ, items)
+		data.writeBakeryOutput(writeFile, skyFQ, items)
 
-    elif dataSet.lower() == "bingo":
+	elif dataSet.lower() == "bingo":
 
-        items = data.buildAuthors("authorlist.psv")
+		items = data.buildAuthors("authorlist.psv")
 
-        fqItmsets = apriori(T, range(len(items)), minSup)
-        skyFQ = skylineFrequentItemsets(fqItmsets)
+		fqItmsets = apriori(T, range(len(items)), minSup)
+		skyFQ = skylineFrequentItemsets(fqItmsets)
 
-        data.writeBingoOutput(writeFile, skyFQ, items)
+		data.writeBingoOutput(writeFile, skyFQ, items)
 
-    return
+	return
 
 
 
